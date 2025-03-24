21a2a0ccf6a3fb8e51f9355cab2fd04baf3716c0
Andrei <Andrei@DESKTOP-8H9VLHH>
2025-03-24 21:47:28 +0100

    Add project files.

---
+++ CompanyAPI/Controllers/CompanyController.cs
@@ -0,0 +1,31 @@
+﻿using CompanyAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyAPI.Controllers
+{
+    //https://localhost:7070 / api / Company / 
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Company>>> GetAll()
+        {
+            var companies = new List<Company> {
+            new Company
+            {
+                Id = 1,
+                Name = "Test",
+                StockTicker = "T1",
+                Exchange = "USD",
+                Isin = "US123",
+                WebsiteUrl = "",
+            }
+            };
+
+            return Ok(companies);
+        }
+    }
+}
---
+++ CompanyAPI/Entities/Company.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanyAPI.Entities
+{
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string StockTicker { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Exchange { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Za-z]{2}[A-Za-z0-9]{10}$", ErrorMessage = "ISIN must start with two letters followed by 10 alphanumeric characters.")]
+        public string Isin { get; set; }
+
+        [Url]
+        public string? WebsiteUrl { get; set; }
+    }
+}
